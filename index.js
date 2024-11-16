5cb722f210674fc496bb616934e0893c53b71aa6
ruthgracety <ruthgracety@gmail.com>
2024-11-16 15:34:21 +0000

    added current location

--- index.js
+++ index.js
@@ -26,7 +26,10 @@ setInterval(function () {
 
 function updateCity(event) {
     let cityTimeZone = event.target.value;
-let cityName = cityTimeZone.replace("_", " ").split("/")[1];
+if (cityTimeZone === "current") {
+cityTimeZone = moment.tz.guess();
+}
+    let cityName = cityTimeZone.replace("_", " ").split("/")[1];
     let cityTime = moment().tz(cityTimeZone);
 let citiesElement = document.querySelector("#cities");
 citiesElement.innerHTML = `
